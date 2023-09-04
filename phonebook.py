760cac52eac782acb92ae0c02fbf3e1c71d30807
Sarkisyan Artak <sarkisyan.artak92@gmail.com>
2023-09-04 11:16:17 +0300

    finished project

--- phonebook.py
+++ phonebook.py
@@ -1,3 +1,9 @@
+import json
+import math
+
+from prettytable import PrettyTable
+
+
 class PhoneBook:
     ''' The Phonebook class is a class in which the following methods will be implemented
     - method for displaying all entries from the directory;
@@ -7,35 +13,64 @@ class PhoneBook:
     '''
 
     def __init__(self, path: str):
-
         self.path = path
-        self.__data = []
+        self.__data: list[dict] = []
+        try:
+            f = open(self.path, 'r')
+            data = f.read()
+            f.close()
+            self.__data = json.loads(data)
+        except FileNotFoundError:
+            pass
 
     def add_entry(self, last_name: str, first_name: str, patronymic: str, company: str, work_phone: int,
                   personal_phone: int):
         entry = {'last_name': last_name, 'first_name': first_name, 'patronymic': patronymic, 'company': company,
                  'work_phone': work_phone, 'personal_phone': personal_phone}
-        f = open('phonebook.txt', 'w', encoding='utf8')
-        f.write(str(self.__data.append(entry)))
-        f.flush()
-        f.close()
+        self.__data.append(entry)
 
-    def show_entries(self, page_number: int):
-        if 1 > page_number > len(self.__data) / 5:
+    def show_entries(self, page_number: int) -> list[list]:
+        if 1 > page_number > math.ceil(len(self.__data) / 5):
             raise ValueError('Please enter a valid page number')
-        table_items = 'last_name', 'first_name', 'patronymic', 'company', 'work_phone', 'personal_phone'
-        table = '\t|\t'.join(table_items)
+        page_entries = []
         start_entry_num = (page_number - 1) * 5
-        if len(self.__data) == 0:
-            f = open('phonebook.txt', 'w', encoding='utf8')
-            f.write(table)
-            f.flush()
-            f.close()
-        else:
-            for entry in self.__data[start_entry_num:start_entry_num + 5]:
-                table += str(entry + 1) + '|' + '\t|\t'.join(self.__data[entry].values())
-                f = open('phonebook.txt', 'w', encoding='utf8')
-                f.write(table)
-                f.flush()
-                f.close()
-        print('Open file phonebook.txt')
+        for index, entry in enumerate(self.__data[start_entry_num:start_entry_num + 5]):
+            data = list(map(str, entry.values()))
+            data.insert(0, index + 1 + start_entry_num)
+            page_entries.append(data)
+        return page_entries
+
+    def search_entry(self, param: str) -> list[list]:
+        found_entries = []
+        for index, entry in enumerate(self.__data):
+            if param in list(entry.values()):
+                data = list(map(str, entry.values()))
+                data.insert(0, str(index + 1))
+                found_entries.append(data)
+        return found_entries
+
+    def as_pretty_table(self, data_list: list[list]) -> PrettyTable:
+        table = PrettyTable()
+        table.field_names = ['№', 'last_name', 'first_name', 'patronymic', 'company', 'work_phone',
+                             'personal_phone']
+        for data in data_list:
+            table.add_row(data)
+        return table
+
+    def edit_entry(self, entry_num: int, new_entry_values: dict):
+        if not (1 <= entry_num <= len(self.__data)):
+            raise ValueError('wrong entry number')
+
+        updating_entry: dict = self.__data[entry_num - 1]
+        updating_entry.update(new_entry_values)
+
+    def get_entry_by_num(self, entry_num: int) -> dict:
+        if not (1 <= entry_num <= len(self.__data)):
+            raise ValueError('wrong entry number')
+
+        return self.__data[entry_num - 1]
+
+    def close(self):
+        f = open('phonebook.json', 'w', encoding='utf8')
+        f.write(json.dumps(self.__data))
+        f.close()
